b4d7f13c08b788b8342b8f95cd2884b7c3e5558e
FraSanta0 <109066301+FraSanta0@users.noreply.github.com>
2022-08-04 11:26:35 +0200

    aggiunte le stanze al server

--- server.js
+++ server.js
@@ -18,34 +18,47 @@ app.get('/', (req,res) => {
 let userList = new Map();
 
 io.on('connection', (socket) => {
-    let userName = socket.handshake.query.userName;
-    addUser(userName, socket.id);
+    let id_account = socket.handshake.query.id_account;
+    let room1;
+    addUser(id_account, socket.id);
 
     socket.broadcast.emit('user-list', [...userList.keys()]);
     socket.emit('user-list', [...userList.keys()]);
 
     socket.on('message', (msg) => {
-        socket.broadcast.emit('message-broadcast', {message: msg, userName: userName});
+        console.log(msg);
+        socket.broadcast.emit('message-broadcast', {message: msg.msg, id_account: id_account, date: msg.date, time: msg.time});
+    })
+
+    socket.on('messageRoom', (msg) => {
+        console.log(msg);
+        socket.in(room1).emit('message2', {message: msg.msg, id_account: id_account, date: msg.date, time: msg.time});
     })
 
     socket.on('disconnect', (reason) => {
-        removeUser(userName, socket.id);
+        removeUser(id_account, socket.id);
     })
+
+    socket.on('create', (room) => {
+        socket.join(room);
+        io.sockets.in(room).emit('connectToRoom', "You are in room no. "+room);
+        room1=room;
+      });
 });
 
-function addUser(userName, id) {
-    if(!userList.has(userName)) {
-        userList.set(userName, new Set(id));
+function addUser(id_account, id) {
+    if(!userList.has(id_account)) {
+        userList.set(id_account, new Set(id));
     } else {
-        userList.get(userName).add(id);
+        userList.get(id_account).add(id);
     }
 }
 
-function removeUser(userName, id) {
-    if(userList.has(userName)) {
-        let userIds = userList.get(userName);
+function removeUser(id_account, id) {
+    if(userList.has(id_account)) {
+        let userIds = userList.get(id_account);
         if(userIds.size == 0){
-            userList.delete(userName);
+            userList.delete(id_account);
         }
     }
 }
